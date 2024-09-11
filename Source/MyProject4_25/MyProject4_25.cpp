be3e0a06d849d8062ab14d3db6a629c3682789a9
Hanks Huang <hanksgml@gmail.com>
2024-09-11 20:35:14 +0800

    update to UE5.4
    update to UE5.4

--- Source/MyProject4_25/MyProject4_25.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MyProject4_25.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyProject4_25, "MyProject4_25" );
- 
